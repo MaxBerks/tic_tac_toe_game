8bf2084605a3984a046c9b002ae51d9b86593adb
Maksym Berkovskyi <00berks.m@gmail.com>
2024-02-27 15:09:47 +0100

    feat: implement functionality for players turn

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Board from "./Board";
 
+enum PlayerTurn {
+    PLAYER_X = 'X',
+    PLAYER_O = 'O',
+}
+
 function TicTacToe() {
+    const [tiles, setTiles] = useState(Array(9).fill(null));
+    const [playerTurn, setPlayerTurn] = useState(PlayerTurn.PLAYER_X)
+
+    const handleTileClick = (index: number) => {
+        if(tiles[index] !== null) return;
+
+        const newTiles = [...tiles];
+        newTiles[index] = playerTurn;
+        setTiles(newTiles);
+        setPlayerTurn((playerTurn === PlayerTurn.PLAYER_X) ? PlayerTurn.PLAYER_O : PlayerTurn.PLAYER_X);
+    }
+
     return (
         <div>
             <h1>Tic Tact Toe</h1>
-            <Board/>
+            <Board tiles={tiles} onTileClick={handleTileClick}/>
         </div>
     );
 }
